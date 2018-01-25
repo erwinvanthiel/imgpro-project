281c32afbf552a3ddf44f5081d27b497a934f090
erwin <E.L.vanthiel@student.tudelft.nl>
2018-01-25 17:18:42 +0100

    no message

---
+++ chars.m
@@ -0,0 +1,29 @@
+im_1 = imread('images/1.jpg');
+im_2 = imread('images/2.jpg');
+im_3 = imread('images/3.jpg');
+im_4 = imread('images/4.jpg');
+im_5 = imread('images/5.jpg');
+im_6 = imread('images/6.jpg');
+im_7 = imread('images/7.jpg');
+im_8 = imread('images/8.jpg');
+im_9 = imread('images/9.jpg');
+im_0 = imread('images/0.jpg');
+im_w = imread('images/w.jpg');
+im_r = imread('images/r.jpg');
+im_t = imread('images/t.jpg');
+im_p = imread('images/p.jpg');
+im_s = imread('images/s.jpg');
+im_d = imread('images/d.jpg');
+im_f = imread('images/f.jpg');
+im_g = imread('images/g.jpg');
+im_h = imread('images/h.jpg');
+im_j = imread('images/j.jpg');
+im_k = imread('images/k.jpg');
+im_l = imread('images/l.jpg');
+im_z = imread('images/z.jpg');
+im_x = imread('images/x.jpg');
+im_v = imread('images/v.jpg');
+im_b = imread('images/b.jpg');
+im_n = imread('images/n.jpg');
+
+
